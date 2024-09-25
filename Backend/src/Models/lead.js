30964f24b766c093f73f82e03973400a2048de31
iAmShivay <shivaysharma77894@gmail.com>
2024-09-25 10:34:35 +0530

    mi fix

--- Backend/src/Models/lead.js
+++ Backend/src/Models/lead.js
@@ -1,48 +1,51 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your name"],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Please enter the email"],
-    trim: true,
-    lowercase: true,
-    validate: {
-      validator: function (v) {
-        return validator.isEmail(v); // Validate email using validator package
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter the email"],
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return validator.isEmail(v); // Validate email using validator package
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
       },
-      message: (props) => `${props.value} is not a valid email address!`,
+    },
+    course: {
+      type: String,
+      required: [true, "Please enter interested course"],
+      trim: true,
+    },
+    college: {
+      type: String,
+      trim: true,
+    },
+    place: {
+      type: String,
+      required: [true, "Please enter the place"],
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Please enter your phone number"],
+      validate: {
+        validator: function (v) {
+          return /^\d{10}$/.test(v); // Assuming a 10 digit phone number format
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
     },
   },
-  course: {
-    type: String,
-    required: [true, "Please enter interested course"],
-    trim: true,
-  },
-  college: {
-    type: String,
-    trim: true,
-  },
-  place: {
-    type: String,
-    required: [true, "Please enter the place"],
-    trim: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: [true, "Please enter your phone number"],
-    validate: {
-      validator: function (v) {
-        return /^\d{10}$/.test(v); // Assuming a 10 digit phone number format
-      },
-      message: (props) => `${props.value} is not a valid phone number!`,
-    },
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Lead", userSchema);
